0db036fa2f74e80fe846919a316c644a3f69815e
Cathy Dao <cathydao95@gmail.com>
2022-06-21 11:29:35 -0700

    add useCallback and functions on click

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,8 +4,6 @@ import Images from "./Images";
 function Carousel() {
   const [imageData, setImageData] = useState("");
 
-  const [imgIndex, setImgIndex] = useState("");
-
   useEffect(() => {
     fetch("https://www.reddit.com/r/aww/top/.json?t=all")
       .then((res) => res.json())
@@ -21,29 +19,9 @@ function Carousel() {
       });
   }, []);
 
-  function nextImg() {
-    console.log(imageData);
-    if (imgIndex < imageData.length < 1) {
-      setImgIndex((prevImgIndex) => prevImgIndex + 1);
-    } else if (imgIndex === imageData.length < 1) {
-      setImgIndex(0);
-    }
-    console.log(imageData[imgIndex]);
-  }
-
-  function previousImg() {
-    if (imgIndex > 0) {
-      setImgIndex((prevImgIndex) => prevImgIndex - 1);
-    }
-  }
-
   return (
-    <div>
-      <Images
-        imageData={imageData}
-        nextImg={nextImg}
-        previousImg={previousImg}
-      />
+    <div className="carousel-container">
+      <Images imageData={imageData} />
     </div>
   );
 }
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,10 +1,40 @@
+import { useState, useEffect, useCallback } from "react";
+
 function Images(props) {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  const nextImg = useCallback(() => {
+    if (imgIndex < props.imageData.length - 1) {
+      setImgIndex((prevImgIndex) => prevImgIndex + 1);
+    } else if (imgIndex === props.imageData.length - 1) {
+      setImgIndex(0);
+    }
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      nextImg();
+    }, 3000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [nextImg]);
+
+  function previousImg() {
+    if (imgIndex > 0) {
+      setImgIndex((prevImgIndex) => prevImgIndex - 1);
+    } else if (imgIndex === 0) {
+      setImgIndex(props.imageData.length - 1);
+    }
+  }
   return (
     <div>
-      <h1>Test</h1>
-      <button onClick={props.previousImg}>Previous Image</button>
-      <img src={props.imageData[4]} alt="test" />
-      <button onClick={props.nextImg}>Next Image</button>
+      <button onClick={previousImg}>Previous Image</button>
+      <div className="image-container">
+        <img className="image" src={props.imageData[imgIndex]} alt="test" />
+      </div>
+
+      <button onClick={nextImg}>Next Image</button>
     </div>
   );
 }
